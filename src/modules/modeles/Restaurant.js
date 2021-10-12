a6f7c7be99b0046640ce305bd6c0404a6e503248
RethersMathieu <71334121+RethersMathieu@users.noreply.github.com>
2021-10-12 18:57:42 +0200

    Amélioration mdels

--- src/modules/modeles/Restaurant.js
+++ src/modules/modeles/Restaurant.js
@@ -1,23 +1,55 @@
-export default class Restaurant {
-    #private_id;
-    #private_name;
-    #private_cuisine;
+import Adresse from "./Adresse";
 
-    constructor(id, name, cuisine) {
-        this.#private_id = id;
-        this.#private_name = name;
-        this.#private_cuisine = cuisine;
+export default class Restaurant {
+    #_id;
+    #_name;
+    #_cuisine;
+    #_grades;
+    #_adresse;
+    #_restaurant_id;
+
+    constructor(id, name, cuisine, grades, adresse, restaurant_id) {
+        this.#_id = id;
+        this.#_name = name;
+        this.#_cuisine = cuisine;
+        this.#_grades = grades;
+        this.#_adresse = adresse;
+        this.#_restaurant_id = restaurant_id;
     }
 
     get id() {
-        return this.#private_id;
+        return this.#_id ?? '';
     }
 
     get name() {
-        return this.#private_name;
+        return this.#_name ?? '';
     }
 
     get cuisine() {
-        return this.#private_cuisine;
+        return this.#_cuisine ?? '';
+    }
+
+    get grades() {
+        return this.#_grades ?? [];
+    }
+
+    get adresse() {
+        return this.#_adresse ?? new Adresse();
+    }
+
+    get restaurant_id() {
+        return this.#_restaurant_id ?? '';
+    }
+
+    static convertTo(obj) {
+        if (!!obj) {
+            return Object.assign(new Restaurant(), obj);
+        }
+    }
+
+    static convertsTo(arr) {
+        if (!!arr) {
+            return arr.map<Restaurant>((obj) => this.convertTo(obj));
+        }
     }
 }
